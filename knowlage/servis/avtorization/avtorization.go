97ac9ffed341fbfbca8d8ead0fd24d3f20918d7a
Anna K <kurbanova.anna@list.ru>
2024-10-29 14:34:18 +0300

    hi

--- knowlage/servis/avtorization/avtorization.go
+++ knowlage/servis/avtorization/avtorization.go
@@ -1,7 +1,22 @@
 package avtorization
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
+type Avtorization struct {
+}
+
+func NewAvtorization() Avtorization {
+	return Avtorization{}
+}
+
+func (das Avtorization) Registration(password string, login string, phoneNumber int) {
+	Registration(password, login, phoneNumber)
+}
+
+// ---
 type Guest struct {
 	name         string
 	ID           int
@@ -18,4 +33,21 @@ var MnogoGuestov []Guest = []Guest{}
 func CreateUser(Name string, ps string, PNumber int) {
 	MnogoGuestov = append(MnogoGuestov, Guest{name: Name, password: ps, phoneNumber: PNumber})
 	fmt.Println(MnogoGuestov)
+}
+func Registration(password string, login string, phoneNumber int) {
+	//var polzovatel Guest= Guest{}
+	polzovatel := Guest{
+		password:    password,
+		name:        login,
+		phoneNumber: phoneNumber,
+	}
+	MnogoGuestov = append(MnogoGuestov, polzovatel)
+
+}
+func VxodKeys(w http.ResponseWriter, r *http.Request) {
+	v := func(string, string) {
+		Registration(password string, login string)
+	}
+	v()
+
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"MiayCoffe/knowlage/servis/avtorization"
+	
 	"encoding/json"
 	"fmt"
 	"html/template"
@@ -12,6 +13,7 @@ func main() {
 	// Указываем путь к папке с файлами
 	fs := http.FileServer(http.Dir("./static"))
 
+
 	// Обрабатываем запросы к "/static/" и перенаправляем на файловый сервер
 	fmt.Println("helloWork")
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
@@ -21,10 +23,12 @@ func main() {
 	http.HandleFunc("/registration", hendlerGuest)
 	http.HandleFunc("/miay2", PodkluchenieHTMLsCC)
 	//http://127.0.0.1:1324/miay2
+	//регистрация пользователя
+	http.HandleFunc("/Registration", KeisDliaAvtorizacii)
 	http.ListenAndServe("127.0.0.1:1324", nil)
 
 }
-func PodkluchenieHTMLsCC (w http.ResponseWriter, r *http.Request){
+func PodkluchenieHTMLsCC(w http.ResponseWriter, r *http.Request) {
 	// Парсим HTML-шаблон
 	tmpl, err := template.ParseFiles("index2.html")
 	if err != nil {
@@ -67,11 +71,31 @@ func hendlerGuest(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-    avtorization.CreateUser(Person.Name, Person.Password, Person.PhoneNumber)
+	avtorization.CreateUser(Person.Name, Person.Password, Person.PhoneNumber)
 }
 
 type DataGuest struct {
 	Name        string
 	Password    string
 	PhoneNumber int
+}
+
+func KeisDliaAvtorizacii(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("KeisDliaAvtorizacii")
+	type Person struct {
+		Login       string
+		Password    string
+		PhoneNumber int
+	}
+	var p Person
+
+	// Try to decode the request body into the struct. If there is an error,
+	// respond to the client with the error message and a 400 status code.
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	avtorization.Registration(p.Login, p.Password, p.PhoneNumber)
+
 }
